45e2ce782b579b58dd3a5021ee8b7fb876aa7a7a
fahri <fahri-can@gmx.at>
2022-06-06 22:05:27 +0200

    Create dto.

---
+++ src/main/kotlin/com/onecosys/get_things_done/dto/TaskDto.kt
@@ -0,0 +1,15 @@
+package com.onecosys.get_things_done.dto
+
+import java.time.LocalDateTime
+
+data class TaskDto(
+        val id: Long,
+        val description: String,
+        val isReminderSet: Boolean,
+        val isTaskOpen: Boolean,
+        val createdOn: LocalDateTime,
+        val startedOn: LocalDateTime,
+        val finishedOn: LocalDateTime,
+        val timeInterval: String,
+        val timeTaken: Int
+)
