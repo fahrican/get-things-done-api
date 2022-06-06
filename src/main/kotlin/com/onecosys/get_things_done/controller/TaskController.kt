3c3bdad7c3317425eb3a48c9f8b8c72622b298b0
fahri <fahri-can@gmx.at>
2022-06-07 00:46:47 +0200

    Add new endpoint to create a task.

--- src/main/kotlin/com/onecosys/get_things_done/controller/TaskController.kt
+++ src/main/kotlin/com/onecosys/get_things_done/controller/TaskController.kt
@@ -1,9 +1,14 @@
 package com.onecosys.get_things_done.controller
 
 import com.onecosys.get_things_done.dto.TaskDto
+import com.onecosys.get_things_done.request.CreateTaskRequest
 import com.onecosys.get_things_done.service.TaskService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid;
+
 
 @RestController
 class TaskController(private val service: TaskService) {
@@ -11,4 +16,10 @@ class TaskController(private val service: TaskService) {
     @GetMapping("/tasks")
     fun getAllTasks(): List<TaskDto> = service.getAllTasks()
 
+
+    @PostMapping("create")
+    fun createStudent(@Valid @RequestBody taskRequest: CreateTaskRequest): TaskDto {
+        val task = service.createTask(taskRequest)
+        return TaskDto(task.taskId, task.description, task.isReminderSet, task.isTaskOpen, task.createdOn, task.startedOn, task.finishedOn, task.timeInterval, task.timeTaken)
+    }
 }
