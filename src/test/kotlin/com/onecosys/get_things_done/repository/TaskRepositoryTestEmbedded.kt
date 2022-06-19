7876e6de276f8f8d6d0ddd01aebdeb34d5080e4c
fahri <fahri-can@gmx.at>
2022-06-19 20:56:47 +0200

    Rename class property.

--- src/test/kotlin/com/onecosys/get_things_done/repository/TaskRepositoryTestEmbedded.kt
+++ src/test/kotlin/com/onecosys/get_things_done/repository/TaskRepositoryTestEmbedded.kt
@@ -2,45 +2,47 @@ package com.onecosys.get_things_done.repository
 
 import com.onecosys.get_things_done.model.Task
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 
-@DataJpaTest
+@DataJpaTest(properties = ["spring.jpa.properties.javax.persistence.validation.mode=none"])
 @ActiveProfiles("test")
 internal class TaskRepositoryTestEmbedded {
 
     @Autowired
-    private lateinit var repository: TaskRepository
+    private lateinit var objectUnderTest: TaskRepository
 
     @Test
     fun `when task is saved then check if it is properly saved`() {
         val task = Task()
-        val savedTask: Task = repository.save(task)
+        val savedTask: Task = objectUnderTest.save(task)
         assertThat(savedTask).usingRecursiveComparison().ignoringFields("id").isEqualTo(task)
     }
 
     @Test
     @Sql("classpath:test-data.sql")
     fun `when task saved through SQL file then check if it is not null`() {
-        val task: Task = repository.findTaskById(111)
+        val task: Task = objectUnderTest.findTaskById(111)
         assertThat(task).isNotNull
     }
 
     @Test
     @Sql("classpath:test-data.sql")
     fun `when task saved through SQL file then check for the number of tasks`() {
-        val tasks: List<Task> = repository.findAll()
+        val tasks: List<Task> = objectUnderTest.findAll()
         assertThat(tasks.size).isEqualTo(2)
     }
 
     @Test
     @Sql("classpath:test-data.sql")
     fun `when task saved through SQL file then remove it by id`() {
-        val tasks: List<Task> = repository.findAll()
-        repository.deleteById(222)
+        val tasks: List<Task> = objectUnderTest.findAll()
+        objectUnderTest.deleteById(222)
         assertThat(tasks.size).isEqualTo(2)
     }
 }
