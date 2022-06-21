19f75df1ca88ff8dc47d567ea733efb084210424
fahri <fahri-can@gmx.at>
2022-06-21 21:04:01 +0200

    Add one more insert statement.

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -7,3 +7,8 @@ INSERT INTO task
 (`id`, `description`, `is_reminder_set`, `is_task_open`, `created_on`, `started_on`, `finished_on`, `time_interval`,
  `time_taken`)
 VALUES (222, 'second todo', false, false, CURRENT_TIME(), CURRENT_TIME(), CURRENT_TIME(), '2d', 2);
+
+INSERT INTO task
+(`id`, `description`, `is_reminder_set`, `is_task_open`, `created_on`, `started_on`, `finished_on`, `time_interval`,
+ `time_taken`)
+VALUES (333, 'third todo', false, true, CURRENT_TIME(), CURRENT_TIME(), CURRENT_TIME(), '2d', 2);
