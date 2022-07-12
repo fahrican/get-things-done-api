b8af634c402718cfb4e41a5a3934e8809c12132a
fahri <fahri-can@gmx.at>
2022-07-12 22:53:43 +0200

    Adapt enum.

--- src/main/kotlin/com/onecosys/get_things_done/model/Priority.kt
+++ src/main/kotlin/com/onecosys/get_things_done/model/Priority.kt
@@ -1,7 +1,5 @@
 package com.onecosys.get_things_done.model
 
-enum class Priority(var status: String) {
-    LOW("low"),
-    MEDIUM("medium"),
-    HIGH("high")
+enum class Priority {
+    LOW, MEDIUM, HIGH
 }
