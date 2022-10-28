58b9f7e19a6fc95adcc77348b468ab151fe8af85
fahri <fahri-can@gmx.at>
2022-10-28 18:59:41 +0200

    Try to fix Unit Tests.

--- src/test/kotlin/com/onecosys/get_things_done/repository/TaskRepositoryTestEmbedded.kt
+++ src/test/kotlin/com/onecosys/get_things_done/repository/TaskRepositoryTestEmbedded.kt
@@ -2,9 +2,11 @@ package com.onecosys.get_things_done.repository
 
 import com.onecosys.get_things_done.entity.Task
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.AssertionsForClassTypes
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 
@@ -61,4 +63,22 @@ internal class TaskRepositoryTestEmbedded {
         val tasks: List<Task> = objectUnderTest.queryAllClosedTasks()
         assertThat(tasks.size).isEqualTo(numberOfClosedTasksInTestDataSql)
     }
+
+    /*
+    @Test
+    fun `when task is saved then check if description is not null and unique`() {
+        // GIVEN
+        val task1 = Task()
+        task1.description = "test"
+        val task2 = Task()
+        task2.description = "test"
+        objectUnderTest.save(task1)
+        // WHEN
+        // THEN
+        AssertionsForClassTypes.assertThatThrownBy { objectUnderTest.save(task2) }
+            .hasMessageContaining("could not execute statement; SQL [n/a]; constraint [\"PUBLIC.UK_NUXJDIQ9O90T2L66B8NYURQ4T_INDEX_2 ON PUBLIC.TASK(DESCRIPTION NULLS FIRST) VALUES ( /* 1 */ 'test' )\"; SQL statement:")
+            .isInstanceOf(DataIntegrityViolationException::class.java)
+    }
+    */
+
 }
