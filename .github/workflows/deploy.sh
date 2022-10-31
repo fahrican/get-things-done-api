3efd390d72389e3f90a58ebf604615d8c590d990
fahri <fahri-can@gmx.at>
2022-10-31 20:40:09 +0100

    Update script.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,6 +3,9 @@
 # Stop script on first error
 set -e
 
+IMAGE_NAME=fahrican/get-things-done
+IMAGE_TAG=latest
+
 echo "${SSH_KEY}" | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
@@ -11,10 +14,10 @@ echo "${SERVER_PUBLIC_KEY}" | base64 -d >> ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ssh_key "fahri@134.209.251.159" \
-  "docker pull fahrican:get-things-done \
+  "docker pull ${IMAGE_NAME}:${IMAGE_TAG} \
   && docker stop live-container \
   && docker rm live-container \
-  && docker run --init -d --name live-container -p 80:3000 fahrican:get-things-done \
+  && docker run --init -d --name live-container -p 9091:9091 ${IMAGE_NAME}:${IMAGE_TAG} \
   && docker system prune -af" # remove unused images to free up space
 
 echo "Successfully deployed, hooray!"
