726ac0d27912fc1f7278f0a349030e64696476b5
fahri <fahri-can@gmx.at>
2022-11-01 12:58:30 +0100

    Adapt script so to use secrets from GitHub according to StackOverflow.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,14 +3,14 @@
 # Stop script on first error
 set -ex -o pipefail
 
-echo "${secrets.SSH_KEY}" | base64 -d > ssh_key
+echo "$SSH_KEY" | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
 # .ssh folder
 mkdir .ssh
 
 # Add production server to known hosts
-echo "${secrets.SERVER_PUBLIC_KEY}" | base64 -d >> ~/.ssh/known_hosts
+echo "$SERVER_PUBLIC_KEY" | base64 -d >> ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ssh_key "fahri@134.209.251.159" \
