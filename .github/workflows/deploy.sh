be30c840e0d87ede1cb96ec0b32fde495f39f291
fahri <fahri-can@gmx.at>
2022-11-01 12:39:24 +0100

    Try to get rif of error message because of variables.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,9 +3,6 @@
 # Stop script on first error
 set -ex -o pipefail
 
-IMAGE_NAME=fahrican/get-things-done
-IMAGE_TAG=latest
-
 echo "${secrets.SSH_KEY}" | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
@@ -17,10 +14,10 @@ echo "${secrets.SERVER_PUBLIC_KEY}" | base64 -d >> ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ssh_key "fahri@134.209.251.159" \
-  "docker pull ${IMAGE_NAME}:${IMAGE_TAG} \
+  "docker pull fahrican/get-things-done:latest \
   && docker stop live-container \
   && docker rm live-container \
-  && docker run --init -d --name live-container -p 9091:9091 ${IMAGE_NAME}:${IMAGE_TAG} \
+  && docker run --init -d --name live-container -p 9091:9091 fahrican/get-things-done:latest \
   && docker system prune -af" # remove unused images to free up space
 
 echo "Successfully deployed, hooray!"
