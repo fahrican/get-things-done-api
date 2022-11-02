d6b787c1242c2dec798f3abd6558b1e284e5d737
fahri <fahri-can@gmx.at>
2022-11-02 01:54:32 +0100

    Adapt container name.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -18,9 +18,9 @@ echo "Deploying via remote SSH"
 
 sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "fahri@134.209.251.159" \
   "docker pull fahrican/get-things-done:latest \
-  && docker stop live-container \
-  && docker rm live-container \
-  && docker run --init -d --name live-container -p 9091:9091 fahrican/get-things-done:latest \
+  && docker stop fahrican/get-things-done \
+  && docker rm fahrican/get-things-done \
+  && docker run --init -d --name fahrican/get-things-done -p 9091:9091 fahrican/get-things-done:latest \
   && docker system prune -af" # remove unused images to free up space
 
 echo "Successfully deployed, hoooooray!"
