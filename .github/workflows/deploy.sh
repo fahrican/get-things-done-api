be80472abf54d1c9d5eb957703499e3dca464832
Fahri Can <fahri-can@gmx.at>
2022-11-07 21:44:57 +0100

    Docker env part two (#11)
    * Use of environment variables.
    
    Co-authored-by: Fahri MacBook <fahri-can@web.de>

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,20 +3,20 @@
 # Stop script on first error
 set -ex -o pipefail
 
-echo "$SSH_KEY" > ssh_key
-chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
-
-# .ssh folder
-mkdir ~/.ssh
-touch ~/.ssh/known_hosts
-chmod 700 ~/.ssh/known_hosts
-
-# Add production server to known hosts
-echo "$SERVER_PUBLIC_KEY" >> ~/.ssh/known_hosts
+#echo "$SSH_KEY" > ssh_key
+#chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
+#
+## .ssh folder
+#mkdir ~/.ssh
+#touch ~/.ssh/known_hosts
+#chmod 700 ~/.ssh/known_hosts
+#
+## Add production server to known hosts
+#echo "$SERVER_PUBLIC_KEY" >> ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 
-sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "fahri@134.209.251.159" \
+sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "$USER_PLUS_DROPLET" \
   "docker pull fahrican/get-things-done:latest \
   && docker stop get-things-done-be \
   && docker rm get-things-done-be \
