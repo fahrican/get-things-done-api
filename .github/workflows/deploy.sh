71cfa6130baa47a9b4d99dc5a57ee492c1f2499f
Fahri Can <fahri-can@gmx.at>
2022-11-07 21:30:57 +0100

    Docker env (#10)
    * Use of environment variables.
    
    Co-authored-by: Fahri MacBook <fahri-can@web.de>

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,7 +20,11 @@ sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "fahri@134.209.251.159" \
   "docker pull fahrican/get-things-done:latest \
   && docker stop get-things-done-be \
   && docker rm get-things-done-be \
-  && docker run -d --name get-things-done-be -p 9091:9091 fahrican/get-things-done:latest \
+  && docker run -d -e SERVER_PORT=$SERVER_PORT \
+                   -e DATASOURCE_URL=$DATASOURCE_URL \
+                   -e DATASOURCE_USERNAME=$DATASOURCE_USERNAME \
+                   -e DATASOURCE_PASSWORD=$DATASOURCE_PASSWORD \
+  --name get-things-done-be -p 9091:9091 fahrican/get-things-done:latest \
   && docker system prune -af" # remove unused images to free up space
 
 echo "Successfully deployed, hoooooray!"
