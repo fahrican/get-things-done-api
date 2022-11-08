82da8b0985b677822360a58c96fc1d48e7482361
Fahri MacBook <fahri-can@web.de>
2022-11-08 20:00:28 +0100

    Remove comments.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,17 +3,6 @@
 # Stop script on first error
 set -ex -o pipefail
 
-#echo "$SSH_KEY" > ssh_key
-#chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
-#
-## .ssh folder
-#mkdir ~/.ssh
-#touch ~/.ssh/known_hosts
-#chmod 700 ~/.ssh/known_hosts
-#
-## Add production server to known hosts
-#echo "$SERVER_PUBLIC_KEY" >> ~/.ssh/known_hosts
-
 echo "Deploying via remote SSH"
 
 sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "$USER_PLUS_DROPLET" \
