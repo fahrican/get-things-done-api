9db29d66534941002ed9f15e6647989dacfa1154
Fahri MacBook <fahri-can@web.de>
2022-11-25 00:33:54 +0100

    Put everything back again.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -12,6 +12,7 @@ sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no "$USER_PLUS_DROPLET" \
   && docker run -d -e SERVER_PORT=$SERVER_PORT \
                    -e JPA_DATABASE=$JPA_DATABASE \
                    -e DATASOURCE_URL=$DATASOURCE_URL \
+                   -e DATASOURCE_DRIVER_CLASS_NAME=$DATASOURCE_DRIVER_CLASS_NAME \
                    -e DATASOURCE_USERNAME=$DATASOURCE_USERNAME \
                    -e DATASOURCE_PASSWORD=$DATASOURCE_PASSWORD \
   --name get-things-done-be -p $SERVER_PORT:$SERVER_PORT fahrican/get-things-done:latest \
