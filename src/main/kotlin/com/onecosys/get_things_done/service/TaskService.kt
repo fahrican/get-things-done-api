57b9a964f0cc4c7bcbe77098320dc3b3976f04dd
Fahri Can <fahri.can@a4o.com>
2023-01-17 23:16:30 +0100

    Use of open closed SOLID principle for mapper and service.

--- src/main/kotlin/com/onecosys/get_things_done/service/TaskService.kt
+++ src/main/kotlin/com/onecosys/get_things_done/service/TaskService.kt
@@ -1,77 +1,22 @@
 package com.onecosys.get_things_done.service
 
 import com.onecosys.get_things_done.data.model.dto.TaskDto
-import com.onecosys.get_things_done.data.entity.Task
 import com.onecosys.get_things_done.data.model.request.TaskCreateRequest
 import com.onecosys.get_things_done.data.model.request.TaskUpdateRequest
-import com.onecosys.get_things_done.exception.BadRequestException
-import com.onecosys.get_things_done.exception.TaskNotFoundException
-import com.onecosys.get_things_done.helper.Mapper
-import com.onecosys.get_things_done.repository.TaskRepository
-import org.springframework.stereotype.Service
-import java.util.stream.Collectors
-import kotlin.reflect.full.memberProperties
-import org.springframework.util.ReflectionUtils
-import java.lang.reflect.Field
 
+interface TaskService {
 
-@Service
-class TaskService(private val repository: TaskRepository) {
+    fun getAllTasks(): List<TaskDto>
 
-    private val mapper = Mapper()
+    fun getAllOpenTasks(): List<TaskDto>
 
-    private fun checkForTaskId(id: Long) {
-        if (!repository.existsById(id)) {
-            throw TaskNotFoundException("Task with ID: $id does not exist!")
-        }
-    }
+    fun getAllClosedTasks(): List<TaskDto>
 
-    fun getAllTasks(): List<TaskDto> =
-        repository.findAll().stream().map { mapper.toDto(it) }.collect(Collectors.toList())
+    fun getTaskById(id: Long): TaskDto
 
-    fun getAllOpenTasks(): List<TaskDto> =
-        repository.queryAllOpenTasks().stream().map { mapper.toDto(it) }.collect(Collectors.toList())
+    fun createTask(createRequest: TaskCreateRequest): TaskDto
 
-    fun getAllClosedTasks(): List<TaskDto> =
-        repository.queryAllClosedTasks().stream().map { mapper.toDto(it) }.collect(Collectors.toList())
+    fun updateTask(id: Long, updateRequest: TaskUpdateRequest): TaskDto
 
-    fun getTaskById(id: Long): TaskDto {
-        checkForTaskId(id)
-        val task: Task = repository.findTaskById(id)
-        return mapper.toDto(task)
-    }
-
-    fun createTask(createRequest: TaskCreateRequest): TaskDto {
-        if (repository.doesDescriptionExist(createRequest.description)) {
-            throw BadRequestException("There is already a task with description: ${createRequest.description}")
-        }
-        val task = Task()
-        mapper.toEntity(createRequest, task)
-        val savedTask = repository.save(task)
-        return mapper.toDto(savedTask)
-    }
-
-    fun updateTask(id: Long, updateRequest: TaskUpdateRequest): TaskDto {
-        checkForTaskId(id)
-        val existingTask: Task = repository.findTaskById(id)
-
-        for (prop in TaskUpdateRequest::class.memberProperties) {
-            if (prop.get(updateRequest) != null) {
-                val field: Field? = ReflectionUtils.findField(Task::class.java, prop.name)
-                field?.let {
-                    it.isAccessible = true
-                    ReflectionUtils.setField(it, existingTask, prop.get(updateRequest))
-                }
-            }
-        }
-
-        val savedTask: Task = repository.save(existingTask)
-        return mapper.toDto(savedTask)
-    }
-
-    fun deleteTask(id: Long): String {
-        checkForTaskId(id)
-        repository.deleteById(id)
-        return "Task with id: $id has been deleted."
-    }
+    fun deleteTask(id: Long): String
 }
