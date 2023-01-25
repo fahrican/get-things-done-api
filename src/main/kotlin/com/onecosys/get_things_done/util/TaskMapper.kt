3a8c71802084a42de3776855f15dddd8f55c3e1c
Fahri Can <fahri-can@gmx.at>
2023-01-25 23:41:33 +0100

    Fix merge problems (#20)
    Fixing merge problems.
    
    Co-authored-by: Fahri Can <fahri.can@a4o.com>

--- src/main/kotlin/com/onecosys/get_things_done/util/TaskMapper.kt
+++ src/main/kotlin/com/onecosys/get_things_done/util/TaskMapper.kt
@@ -3,10 +3,11 @@ package com.onecosys.get_things_done.util
 import com.onecosys.get_things_done.data.entity.Task
 import com.onecosys.get_things_done.data.model.dto.TaskDto
 import com.onecosys.get_things_done.data.model.request.TaskCreateRequest
+import java.time.Clock
 
 interface TaskMapper {
 
     fun toDto(entity: Task): TaskDto
 
-    fun toEntity(request: TaskCreateRequest, entity: Task): Task
+    fun toEntity(request: TaskCreateRequest, clock: Clock, entity: Task): Task
 }
