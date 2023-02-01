d9ae62ee71da7e32096609047e76b0114bb77086
Fahri Can <fahri.can@a4o.com>
2023-02-01 20:43:19 +0100

    Refactor exception class.

--- src/main/kotlin/com/onecosys/get_things_done/error_handling/TaskExceptionHandler.kt
+++ src/main/kotlin/com/onecosys/get_things_done/error_handling/TaskExceptionHandler.kt
@@ -4,30 +4,31 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.client.HttpClientErrorException
 
 @ControllerAdvice
-class TaskException {
+class TaskExceptionAdvice {
 
     @ExceptionHandler(BadRequestException::class)
     fun handleBadRequestException(badRequestException: BadRequestException): ResponseEntity<ApiError> {
-        val error = ApiError(message = badRequestException.message, status = badRequestException.statusCode)
+        val error = ApiError(message = badRequestException.message, status = HttpStatus.BAD_REQUEST)
         return ResponseEntity(error, error.status)
     }
 
     @ExceptionHandler(TaskNotFoundException::class)
     fun handleTaskNotFoundException(taskNotFoundException: TaskNotFoundException): ResponseEntity<ApiError> {
-        val error = ApiError(message = taskNotFoundException.message, status = taskNotFoundException.statusCode)
+        val error = ApiError(message = taskNotFoundException.message, status = HttpStatus.NOT_FOUND)
         return ResponseEntity(error, error.status)
     }
 
     @ExceptionHandler(Exception::class)
     fun handleGeneralException(exception: Exception): ResponseEntity<ApiError> {
         val error = ApiError(message = exception.message, status = HttpStatus.INTERNAL_SERVER_ERROR)
+        println("An exception occurred: ${exception.message}")
+        exception.printStackTrace()
         return ResponseEntity(error, error.status)
     }
 }
 
-data class TaskNotFoundException(override val message: String) : HttpClientErrorException(HttpStatus.NOT_FOUND, message)
+data class TaskNotFoundException(override val message: String) : RuntimeException(message)
 
-data class BadRequestException(override val message: String) : HttpClientErrorException(HttpStatus.BAD_REQUEST, message)
+data class BadRequestException(override val message: String) : RuntimeException(message)
