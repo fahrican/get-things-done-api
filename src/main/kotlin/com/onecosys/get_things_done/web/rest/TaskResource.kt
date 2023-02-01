7a5229347e47bd957055814d8777669a91924eb3
Fahri Can <fahri.can@a4o.com>
2023-02-01 19:26:52 +0100

    Use of only needed imports.

--- src/main/kotlin/com/onecosys/get_things_done/web/rest/TaskResource.kt
+++ src/main/kotlin/com/onecosys/get_things_done/web/rest/TaskResource.kt
@@ -6,7 +6,16 @@ import com.onecosys.get_things_done.model.request.TaskUpdateRequest
 import com.onecosys.get_things_done.service.TaskService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.RequestParam
 import javax.validation.Valid
 
 @CrossOrigin
@@ -28,13 +37,13 @@ class TaskResource(private val service: TaskService) {
 
     @PostMapping
     fun createTask(
-            @Valid @RequestBody createRequest: TaskCreateRequest
+        @Valid @RequestBody createRequest: TaskCreateRequest
     ): ResponseEntity<TaskDto> = ResponseEntity(service.createTask(createRequest), HttpStatus.CREATED)
 
     @PatchMapping("{id}")
     fun updateTask(
-            @PathVariable id: Long,
-            @Valid @RequestBody updateRequest: TaskUpdateRequest
+        @PathVariable id: Long,
+        @Valid @RequestBody updateRequest: TaskUpdateRequest
     ): ResponseEntity<TaskDto> = ResponseEntity.ok(service.updateTask(id, updateRequest))
 
     @DeleteMapping("{id}")
