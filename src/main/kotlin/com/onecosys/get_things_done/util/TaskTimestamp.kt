6564e534215b21cc06dd66cab28f7170d4e3c478
Fahri Can <fahri.can@a4o.com>
2023-02-02 12:09:39 +0100

    Use of ZoneId field to be able to change zone if necessary.

--- src/main/kotlin/com/onecosys/get_things_done/util/TaskTimestamp.kt
+++ src/main/kotlin/com/onecosys/get_things_done/util/TaskTimestamp.kt
@@ -2,9 +2,10 @@ package com.onecosys.get_things_done.util
 
 import org.springframework.stereotype.Component
 import java.time.Clock
+import java.time.ZoneId
 
 @Component
-class TaskTimestamp {
+class TaskTimestamp(private val zoneId: ZoneId = ZoneId.systemDefault()) {
 
-    fun createClockWithZone(): Clock = Clock.systemDefaultZone()
+    fun createClockWithZone(): Clock = Clock.system(zoneId)
 }
