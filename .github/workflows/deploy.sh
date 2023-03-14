5c5b7cf41ddbd4ee86444b70ca7c75f8edad8861
Fahri Can <fahri.can@a4o.com>
2023-03-14 19:53:33 +0100

    Adapt again something.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,6 +6,6 @@ set -ex -o pipefail
 echo "Deploying via remote SSH"
 
 sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no -p "$DROPLET_PORT" "$USER_PLUS_DROPLET" \
-  "cd .github/workflows && docker-compose pull && docker-compose up -d && docker system prune -af"
+  "docker-compose pull && docker-compose up -d && docker system prune -af"
 
 echo "Successfully deployed, hoooooray!"
