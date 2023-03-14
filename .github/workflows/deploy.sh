0a03fd0dd29af54b7e412cb1783d41ea5c9c49c6
Fahri Can <fahri.can@a4o.com>
2023-03-14 19:19:28 +0100

    Adapt things.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,16 +6,6 @@ set -ex -o pipefail
 echo "Deploying via remote SSH"
 
 sshpass -p "$USER_PW" ssh -o StrictHostKeyChecking=no -p "$DROPLET_PORT" "$USER_PLUS_DROPLET" \
-  "docker pull fahrican/get-things-done:latest \
-  && docker stop get-things-done-be \
-  && docker rm get-things-done-be \
-  && docker run -d -e SERVER_PORT=$SERVER_PORT \
-                   -e JPA_DATABASE=$JPA_DATABASE \
-                   -e DATASOURCE_URL=$DATASOURCE_URL \
-                   -e DATASOURCE_DRIVER_CLASS_NAME=$DATASOURCE_DRIVER_CLASS_NAME \
-                   -e DATASOURCE_USERNAME=$DATASOURCE_USERNAME \
-                   -e DATASOURCE_PASSWORD=$DATASOURCE_PASSWORD \
-  --name get-things-done-be -p $SERVER_PORT:$SERVER_PORT fahrican/get-things-done:latest \
-  && docker system prune -af" # remove unused images to free up space
+  "cd .github/workflows && docker-compose pull && docker-compose up -d && docker system prune -af"
 
 echo "Successfully deployed, hoooooray!"
