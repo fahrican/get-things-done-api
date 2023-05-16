827f2220eba58c1ad79e5a818a6afbca9604b6d9
Fahri Can <fahri-can@gmx.at>
2023-05-16 02:14:44 +0200

    Improve TaskTimestamp implementation and test.

--- src/main/kotlin/com/onecosys/getthingsdone/util/TaskTimestamp.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/util/TaskTimestamp.kt
@@ -5,7 +5,7 @@ import java.time.Clock
 import java.time.ZoneId
 
 @Component
-class TaskTimestamp(private val zoneId: ZoneId = ZoneId.systemDefault()) {
+class TaskTimestamp(private val zoneId: ZoneId? = ZoneId.systemDefault()) {
 
-    fun createClockWithZone(): Clock = Clock.system(zoneId)
+    fun createClockWithZone(): Clock = Clock.system(zoneId ?: ZoneId.systemDefault())
 }
--- src/test/kotlin/com/onecosys/getthingsdone/util/TaskTimestampTest.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/util/TaskTimestampTest.kt
@@ -29,4 +29,34 @@ class TaskTimestampTest {
         assertThat(zonedDateTime.zone).isEqualTo(zoneId)
         assertThat(clock.instant()).isEqualTo(instant)
     }
+
+    @Test
+    fun `when clock with different zone is created then expect specific zone id`() {
+        val differentZoneId = ZoneId.of("Europe/Paris")
+        val taskTimestamp = TaskTimestamp(differentZoneId)
+        val clock = taskTimestamp.createClockWithZone()
+
+        assertThat(clock).isNotNull
+        assertThat(clock.zone).isEqualTo(differentZoneId)
+    }
+
+    @Test
+    fun `when clock with non-default zone is created then expect non-default zone id`() {
+        val nonDefaultZoneId = ZoneId.of("Asia/Kolkata")
+        val taskTimestamp = TaskTimestamp(nonDefaultZoneId)
+        val clock = taskTimestamp.createClockWithZone()
+
+        assertThat(clock).isNotNull
+        assertThat(clock.zone).isNotEqualTo(ZoneId.systemDefault())
+        assertThat(clock.zone).isEqualTo(nonDefaultZoneId)
+    }
+
+    @Test
+    fun `when clock with null zone is created then expect default zone id`() {
+        val taskTimestamp = TaskTimestamp(null)
+        val clock = taskTimestamp.createClockWithZone()
+
+        assertThat(clock).isNotNull
+        assertThat(clock.zone).isEqualTo(ZoneId.systemDefault())
+    }
 }
