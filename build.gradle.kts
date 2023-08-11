305809c632e8a144614678900901bb23cb335a92
Fahri Can <fahri-can@gmx.at>
2023-08-11 19:51:36 +0200

    Increase dependency version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.5"
-    id("io.spring.dependency-management") version "1.1.0"
+    id("org.springframework.boot") version "3.0.9"
+    id("io.spring.dependency-management") version "1.1.1"
     id("org.sonarqube") version "3.5.0.2730"
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.spring") version "1.8.21"
