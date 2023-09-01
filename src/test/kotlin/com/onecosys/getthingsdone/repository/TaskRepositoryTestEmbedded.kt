ad9c8fffbbbbda293365710273cb13913e647300
Fahri Can <fahri-can@gmx.at>
2023-09-01 20:53:11 +0200

    Get rid of one application file.

--- src/test/kotlin/com/onecosys/getthingsdone/repository/TaskRepositoryTestEmbedded.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/repository/TaskRepositoryTestEmbedded.kt
@@ -6,10 +6,8 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.jdbc.Sql
 
-@ActiveProfiles("test")
 @DataJpaTest(properties = ["spring.jpa.properties.javax.persistence.validation.mode=none"])
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 internal class TaskRepositoryTestEmbedded {
