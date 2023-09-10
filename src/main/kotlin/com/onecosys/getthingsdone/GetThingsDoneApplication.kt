7ab9a646210bdffcb481b71922ba2b420c96ebe1
Fahri Can <fahri-can@gmx.at>
2023-09-10 20:11:11 +0200

    Use API First approach. (#61)
    * Use of API First approach, documented API and updated GitHub Workers.

--- src/main/kotlin/com/onecosys/getthingsdone/GetThingsDoneApplication.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/GetThingsDoneApplication.kt
@@ -2,15 +2,22 @@ package com.onecosys.getthingsdone
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @OpenAPIDefinition(
     info = Info(
-        title = "Task Management API",
+        title = "Task Management API v1",
         version = "1.0.0",
-        description = "API for managing tasks"
+        description =
+        "This API exposes RESTful CRUD operations for tracking tasks.\n" +
+                "\n" +
+                "Some useful links:\n" +
+                "- [Web UI](https://justluxurylifestyle.com/app/open-tasks)\n" +
+                "- [GitHUb source code](https://github.com/fahrican/get-things-done-api)",
+        license = License(name = "MIT License", url = "https://github.com/fahrican/get-things-done-api/LICENSE.txt")
     )
 )
 class GetThingsDoneApplication
