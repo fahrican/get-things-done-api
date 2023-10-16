cddfff30a1dea8f6e802be5588e5e19b14041a72
Fahri Can <fahri-can@gmx.at>
2023-10-16 23:57:31 +0200

    chore: Remove unused imports.

--- src/main/kotlin/com/onecosys/getthingsdone/config/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/config/JwtAuthenticationFilter.kt
@@ -1,10 +1,8 @@
 package com.onecosys.getthingsdone.config
 
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletException
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.lang.NonNull
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
@@ -12,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
 
 @Component
 class JwtAuthenticationFilter(
