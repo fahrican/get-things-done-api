5f509de6cb5f5cc22d8537c83ea0b6a1dac78194
Fahri Can <fahri-can@gmx.at>
2023-10-20 01:26:51 +0200

    refactor: Improve readability.

--- src/main/kotlin/com/onecosys/getthingsdone/authentication/service/AuthenticationService.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/authentication/service/AuthenticationService.kt
@@ -10,6 +10,7 @@ import com.onecosys.getthingsdone.authorization.UserRepository
 import com.onecosys.getthingsdone.config.JwtService
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -22,15 +23,17 @@ class AuthenticationService(
 ) {
 
     fun register(request: RegisterRequest): AuthenticationResponse {
-        val existingUser: User? = repository.findByEmail(request.email)
-        if (existingUser != null) throw SignUpException("User email already exists!")
+        repository.findByEmail(request.email)?.let {
+            throw SignUpException("User email already exists!")
+        }
 
-        val user = User()
-        user.firstName = request.firstName
-        user.lastName = request.lastName
-        user.email = request.email
-        user.userPassword = passwordEncoder.encode(request.password)
-        user.role = Role.USER
+        val user = User().apply {
+            firstName = request.firstName
+            lastName = request.lastName
+            email = request.email
+            userPassword = passwordEncoder.encode(request.password)
+            role = Role.USER
+        }
 
         repository.save(user)
         val jwtToken = jwtService.generateToken(user)
@@ -39,9 +42,8 @@ class AuthenticationService(
 
     fun authenticate(request: AuthenticationRequest): AuthenticationResponse {
         authenticationManager.authenticate(UsernamePasswordAuthenticationToken(request.email, request.password))
-        val user: User? = repository.findByEmail(request.email)
-        var jwToken = ""
-        user?.let { jwToken = jwtService.generateToken(user) }
-        return AuthenticationResponse(jwToken)
+        val user: User = repository.findByEmail(request.email) ?: throw UsernameNotFoundException("User not found")
+        val jwtToken = jwtService.generateToken(user)
+        return AuthenticationResponse(jwtToken)
     }
 }
