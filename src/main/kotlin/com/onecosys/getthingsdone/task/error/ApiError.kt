b2bb184292d03cad72a2436016c6a3808149618a
Fahri Can <fahri-can@gmx.at>
2023-10-28 16:37:21 +0200

    chore: Improve error messages.

--- src/main/kotlin/com/onecosys/getthingsdone/task/error/ApiError.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/task/error/ApiError.kt
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
 data class ApiError(
-    val message: String? = "Something went wrong",
+    val message: String? = "Something API related went wrong",
     val status: HttpStatus,
     val code: Int = status.value(),
     val timestamp: LocalDateTime = LocalDateTime.now()
--- src/main/kotlin/com/onecosys/getthingsdone/user/error/UserError.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/user/error/UserError.kt
@@ -1,10 +1,10 @@
-package com.onecosys.getthingsdone.authentication.error
+package com.onecosys.getthingsdone.user.error
 
 import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
-data class AuthenticationError(
-    val message: String? = "Something authentication related went wrong",
+data class UserError(
+    val message: String? = "Something user related went wrong",
     val status: HttpStatus,
     val code: Int = status.value(),
     val timestamp: LocalDateTime = LocalDateTime.now()
