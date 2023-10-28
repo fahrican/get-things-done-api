72ff33235b0af27e394cbe2b57c8e0d1011f4366
Fahri Can <fahri-can@gmx.at>
2023-10-28 15:39:13 +0200

    chore: Improve readability.

--- src/main/kotlin/com/onecosys/getthingsdone/authentication/service/JwtServiceImpl.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/authentication/service/JwtServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.onecosys.getthingsdone.authentication.service
 
+import com.onecosys.getthingsdone.authentication.error.JwtAuthenticationException
 import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.io.Decoders
@@ -7,7 +8,8 @@ import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
-import java.util.*
+import java.util.Date
+import java.util.concurrent.TimeUnit
 import javax.crypto.SecretKey
 
 @Service
@@ -21,43 +23,40 @@ class JwtServiceImpl : JwtService {
     }
 
     companion object {
-        private const val EXPIRATION_AFTER_ONE_DAY: Long = 1000 * 60 * 60 * 24
-        private const val EXPIRATION_AFTER_SEVEN_DAYS: Long = 1000 * 60 * 60 * 24 * 7
+        private val EXPIRATION_ONE_DAY = TimeUnit.DAYS.toMillis(1)
+        private val EXPIRATION_SEVEN_DAYS = TimeUnit.DAYS.toMillis(7)
     }
 
     override fun extractUsername(token: String): String = extractAllClaims(token).subject
 
-    override fun isTokenValid(token: String, userDetails: UserDetails): Boolean {
-        return (extractUsername(token) == userDetails.username) && !isTokenExpired(token)
-    }
+    override fun isTokenValid(token: String, userDetails: UserDetails): Boolean =
+        extractUsername(token) == userDetails.username && !isTokenExpired(token)
 
     override fun generateAccessToken(userDetails: UserDetails): String =
-        generateToken(HashMap(), userDetails, EXPIRATION_AFTER_ONE_DAY)
+        generateToken(emptyMap(), userDetails, EXPIRATION_ONE_DAY)
 
+    override fun generateRefreshToken(userDetails: UserDetails): String =
+        generateToken(emptyMap(), userDetails, EXPIRATION_SEVEN_DAYS)
 
-    override fun generateRefreshToken(userDetails: UserDetails): String {
-        val claims = HashMap<String, Any>()
-        return generateToken(claims, userDetails, EXPIRATION_AFTER_SEVEN_DAYS)
-    }
-
-    private fun extractAllClaims(token: String): Claims = Jwts.parser()
-        .verifyWith(secretKey)
-        .build()
-        .parseSignedClaims(token)
-        .payload
-
+    private fun extractAllClaims(token: String): Claims =
+        try {
+            Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .payload
+        } catch (e: Exception) {
+            throw JwtAuthenticationException("Failed to extract claims from token.", e)
+        }
 
     private fun isTokenExpired(token: String): Boolean = extractAllClaims(token).expiration.before(Date())
 
-    private fun generateToken(
-        extractClaims: Map<String, Any>,
-        userDetails: UserDetails,
-        expirationTime: Long
-    ): String = Jwts.builder()
-        .claims(extractClaims)
-        .subject(userDetails.username)
-        .issuedAt(Date(System.currentTimeMillis()))
-        .expiration(Date(System.currentTimeMillis() + expirationTime))
-        .signWith(secretKey)
-        .compact()
+    private fun generateToken(claims: Map<String, Any>, userDetails: UserDetails, expirationTime: Long): String =
+        Jwts.builder()
+            .claims(claims)
+            .subject(userDetails.username)
+            .issuedAt(Date(System.currentTimeMillis()))
+            .expiration(Date(System.currentTimeMillis() + expirationTime))
+            .signWith(secretKey)
+            .compact()
 }
