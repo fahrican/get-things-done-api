d0ed7d9e3393c4999989ba96757f4f80ae7dbabc
Fahri Can <fahri-can@gmx.at>
2023-10-28 15:45:36 +0200

    chore: Improve readability.

--- src/main/kotlin/com/onecosys/getthingsdone/authentication/service/LogoutService.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/authentication/service/LogoutService.kt
@@ -1,9 +1,9 @@
 package com.onecosys.getthingsdone.authentication.service
 
 import com.onecosys.getthingsdone.authorization.TokenRepository
-import com.onecosys.getthingsdone.authorization.model.Token
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.logout.LogoutHandler
@@ -12,22 +12,23 @@ import org.springframework.stereotype.Service
 @Service
 class LogoutService(private val repository: TokenRepository) : LogoutHandler {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     companion object {
         const val AUTH_HEADER = "Authorization"
         const val BEARER_TOKEN_PREFIX = "Bearer "
     }
 
     override fun logout(request: HttpServletRequest?, response: HttpServletResponse?, authentication: Authentication?) {
-        val authHeader: String? = request?.getHeader(AUTH_HEADER)
-        if (authHeader?.startsWith(BEARER_TOKEN_PREFIX) == true) {
-            val jwt: String = authHeader.drop(BEARER_TOKEN_PREFIX.length)
-            val storedToken: Token? = repository.findByToken(jwt)
-            storedToken?.apply {
-                this.expired = true
-                this.revoked = true
-                repository.save(storedToken)
-                SecurityContextHolder.clearContext()
-            }
+        val authHeader: String = request?.getHeader(AUTH_HEADER) ?: return
+        if (authHeader.startsWith(BEARER_TOKEN_PREFIX)) {
+            val jwt: String = authHeader.substring(BEARER_TOKEN_PREFIX.length)
+            repository.findByToken(jwt)?.let { token ->
+                token.expired = true
+                token.revoked = true
+                repository.save(token)
+            } ?: run { log.info("Token not found in database: $jwt") }
         }
+        SecurityContextHolder.clearContext()
     }
 }
