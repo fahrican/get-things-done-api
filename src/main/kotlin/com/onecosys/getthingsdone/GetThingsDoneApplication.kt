97d70a642920815f77bf2c20f247ce246202ac90
Fahri Can <fahri-can@gmx.at>
2023-10-31 20:16:33 +0100

    chore: Update Swagger documentation.

--- src/main/kotlin/com/onecosys/getthingsdone/GetThingsDoneApplication.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/GetThingsDoneApplication.kt
@@ -1,8 +1,13 @@
 package com.onecosys.getthingsdone
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -17,8 +22,34 @@ import org.springframework.boot.runApplication
                 "Some useful links:\n" +
                 "- [Web UI](https://justluxurylifestyle.com/app/open-tasks)\n" +
                 "- [GitHUb source code](https://github.com/fahrican/get-things-done-api)",
-        license = License(name = "MIT License", url = "https://github.com/fahrican/get-things-done-api/blob/master/LICENSE.txt")
-    )
+        license = License(
+            name = "MIT License",
+            url = "https://github.com/fahrican/get-things-done-api/blob/master/LICENSE.txt"
+        )
+    ),
+    servers = [
+        Server(
+            description = "DEV ENV",
+            url = "http://localhost:9091"
+        ),
+        Server(
+            description = "PROD ENV",
+            url = "https://task-manager.justluxurylifestyle.com/"
+        ),
+    ],
+    security = [
+        SecurityRequirement(
+            name = "bearerAuth"
+        )
+    ]
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    description = "JWT authentication description",
+    scheme = "Bearer Token",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    `in` = SecuritySchemeIn.HEADER
 )
 class GetThingsDoneApplication
 
--- src/main/kotlin/com/onecosys/getthingsdone/authentication/web/rest/AuthenticationController.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/authentication/web/rest/AuthenticationController.kt
@@ -4,6 +4,12 @@ import com.onecosys.getthingsdone.authentication.dto.AuthenticationRequest
 import com.onecosys.getthingsdone.authentication.dto.AuthenticationResponse
 import com.onecosys.getthingsdone.authentication.dto.RegisterRequest
 import com.onecosys.getthingsdone.authentication.service.AuthenticationService
+import com.onecosys.getthingsdone.error.ApiError
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,11 +20,49 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v1/auth")
 class AuthenticationController(private val service: AuthenticationService) {
 
+    @Operation(summary = "sign-up user", tags = ["authentication"])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "sign-up was successful",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = AuthenticationResponse::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "409", description = "Invalid sign-up", content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = ApiError::class)
+                )]
+            ),
+        ]
+    )
     @PostMapping("sign-up")
     fun signUp(@RequestBody request: RegisterRequest): ResponseEntity<AuthenticationResponse> =
         ResponseEntity.ok(service.signUp(request))
 
 
+    @Operation(summary = "sign-in user", tags = ["authentication"])
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "sign-in was successful",
+                content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = AuthenticationResponse::class)
+                )]
+            ),
+            ApiResponse(
+                responseCode = "401", description = "Invalid sign-in", content = [Content(
+                    mediaType = "application/json",
+                    schema = Schema(implementation = ApiError::class)
+                )]
+            ),
+        ]
+    )
     @PostMapping("sign-in")
     fun signIn(@RequestBody request: AuthenticationRequest): ResponseEntity<AuthenticationResponse> =
         ResponseEntity.ok(service.signIn(request))
