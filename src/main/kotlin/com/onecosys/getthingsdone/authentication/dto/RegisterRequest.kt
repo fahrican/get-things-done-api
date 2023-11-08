ce2fd12bdb8af57ccfe8e88557dfff67b861c3c8
Fahri Can <fahri-can@gmx.at>
2023-11-08 21:51:00 +0100

    chore: Make sure request fields are not empty and proper email gets provided by user.

--- src/main/kotlin/com/onecosys/getthingsdone/authentication/dto/RegisterRequest.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/authentication/dto/RegisterRequest.kt
@@ -1,10 +1,24 @@
 package com.onecosys.getthingsdone.authentication.dto
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
 data class RegisterRequest(
+    @field:NotBlank
     val firstName: String,
+
+    @field:NotBlank
     val lastName: String,
+
+    @field:Email
     val email: String,
+
+    @field:NotBlank
     val username: String,
+
+    @field:NotBlank
     val password: String,
+
+    @field:NotBlank
     val passwordConfirmation: String,
 )
