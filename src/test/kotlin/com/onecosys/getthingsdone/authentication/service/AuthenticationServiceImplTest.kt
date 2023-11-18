d0c34ae94e7991749ff2f4e001f42298ee3fbf2f
Fahri Can <fahri-can@gmx.at>
2023-11-18 21:46:34 +0100

    chore: Adapt test method.

--- src/test/kotlin/com/onecosys/getthingsdone/authentication/service/AuthenticationServiceImplTest.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/authentication/service/AuthenticationServiceImplTest.kt
@@ -4,10 +4,6 @@ import com.onecosys.getthingsdone.authentication.dto.RegisterRequest
 import com.onecosys.getthingsdone.authentication.dto.VerificationToken
 import com.onecosys.getthingsdone.authentication.repository.VerificationTokenRepository
 import com.onecosys.getthingsdone.authentication.util.UserRegistrationMapper
-import com.onecosys.getthingsdone.task.model.Priority
-import com.onecosys.getthingsdone.task.model.dto.TaskCreateRequest
-import com.onecosys.getthingsdone.task.model.entity.Task
-import com.onecosys.getthingsdone.task.service.TaskServiceImpl
 import com.onecosys.getthingsdone.user.model.entity.User
 import com.onecosys.getthingsdone.user.repository.UserRepository
 import io.mockk.MockKAnnotations
@@ -15,40 +11,38 @@ import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.password.PasswordEncoder
-import java.time.Clock
 import java.time.Instant
-import java.time.ZoneId
 import java.time.temporal.ChronoUnit
 
 @ExtendWith(MockKExtension::class)
 internal class AuthenticationServiceImplTest {
 
     @RelaxedMockK
-    private lateinit var passwordEncoder: PasswordEncoder
+    private lateinit var mockPasswordEncoder: PasswordEncoder
 
     @RelaxedMockK
-    private lateinit var jwtService: JwtService
+    private lateinit var mockJwtService: JwtService
 
     @RelaxedMockK
-    private lateinit var authenticationManager: AuthenticationManager
+    private lateinit var mockAuthenticationManager: AuthenticationManager
 
     @RelaxedMockK
-    private lateinit var mapper: UserRegistrationMapper
+    private lateinit var mockMapper: UserRegistrationMapper
 
     @RelaxedMockK
-    private lateinit var userRepository: UserRepository
+    private lateinit var mockUserRepository: UserRepository
 
     @RelaxedMockK
-    private lateinit var verificationTokenRepository: VerificationTokenRepository
+    private lateinit var mockVerificationTokenRepository: VerificationTokenRepository
 
     @RelaxedMockK
-    private lateinit var emailService: EmailService
+    private lateinit var mockEmailService: EmailService
 
     private lateinit var objectUnderTest: AuthenticationService
 
@@ -57,47 +51,19 @@ internal class AuthenticationServiceImplTest {
         MockKAnnotations.init(this)
 
         objectUnderTest = AuthenticationServiceImpl(
-            passwordEncoder,
-            jwtService,
-            authenticationManager,
-            mapper,
-            userRepository,
-            verificationTokenRepository,
-            emailService
+            mockPasswordEncoder,
+            mockJwtService,
+            mockAuthenticationManager,
+            mockMapper,
+            mockUserRepository,
+            mockVerificationTokenRepository,
+            mockEmailService
         )
     }
 
 
-    /*    @Test
-        fun signUp_shouldSaveUser_InitiateVerificationToken_AndSendEmail() {
-            val request = RegisterRequest("testuser", "test@email.com", "password")
-            val savedUser = mockk()
-            val verificationToken = mock(VerificationToken::class.java)
-
-            `when`(mockMapper.toEntity(request, mockPasswordEncoder)).thenReturn(savedUser)
-            `when`(mockUserRepository.save(savedUser)).thenReturn(savedUser)
-            `when`(authenticationService.initiateEmailVerificationToken(savedUser)).thenReturn(
-                Pair(
-                    "test-token",
-                    verificationToken
-                )
-            )
-
-            val response = authenticationService.signUp(request)
-
-            assertEquals(
-                "Please, check your emails and spam/junk folder for test@email.com to verify your account",
-                response.message
-            )
-            verify(mockUserRepository).save(savedUser)
-            verify(mockVerificationTokenRepository).save(verificationToken)
-            verify(mockEmailService).sendVerificationEmail(savedUser, "test-token")
-        }*/
-
-
     @Test
-    fun `signUp should save user, create verification token, send email, and return confirmation response`() {
-        // Arrange
+    fun `when user sign up is triggerred then expect success message `() {
         val request = RegisterRequest("John", "Doe", "john@example.com", "john-doe", "password", "password")
         val user = User(
             firstName = request.firstName,
@@ -112,23 +78,19 @@ internal class AuthenticationServiceImplTest {
             user = user,
             expiryDate = Instant.now().plus(1, ChronoUnit.DAYS)
         )
+        every { mockUserRepository.findBy_username(any()) } returns null
+        every { mockUserRepository.findByEmail(any()) } returns null
+        every { mockMapper.toEntity(any(), any()) } returns user
+        every { mockUserRepository.save(any()) } returns user
+        every { mockVerificationTokenRepository.save(any()) } returns verificationToken
 
-        every { userRepository.findBy_username(any()) } returns null
-        every { userRepository.findByEmail(any()) } returns null
-        every { mapper.toEntity(any(), any()) } returns user
-        every { userRepository.save(any()) } returns user
-        every { verificationTokenRepository.save(any()) } returns verificationToken
-        every { emailService.sendVerificationEmail(user, token) } returns Unit
-
-        // Act
         val result = objectUnderTest.signUp(request)
 
-        // Assert
         assertEquals(
             "Please, check your emails and spam/junk folder for ${user.email} to verify your account",
             result.message
         )
-        verify { userRepository.save(user) }
-        verify { verificationTokenRepository.save(any()) }
+        verify(exactly = 1) { mockUserRepository.save(user) }
+        verify(exactly = 1) { mockVerificationTokenRepository.save(any()) }
     }
 }
