691825ca6bbe287fabd0cb730c06283fcd34961c
Fahri Can <fahri-can@gmx.at>
2023-11-25 19:00:37 +0100

    chore: Write integration tests for auth controller.

--- src/test/kotlin/com/onecosys/getthingsdone/authentication/web/rest/AuthenticationControllerTest.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/authentication/web/rest/AuthenticationControllerTest.kt
@@ -1,6 +1,8 @@
 package com.onecosys.getthingsdone.authentication.web.rest
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.onecosys.getthingsdone.authentication.dto.AuthenticationRequest
+import com.onecosys.getthingsdone.authentication.dto.AuthenticationResponse
 import com.onecosys.getthingsdone.authentication.dto.EmailConfirmedResponse
 import com.onecosys.getthingsdone.authentication.dto.RegisterRequest
 import com.onecosys.getthingsdone.authentication.service.AuthenticationService
@@ -58,7 +60,55 @@ internal class AuthenticationControllerTest {
 
         resultActions.andExpect(MockMvcResultMatchers.status().isCreated)
         resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
     }
 
+    @Test
+    fun `when user verify is triggered then expect success response`() {
+        val token = "a1b2c3"
+        `when`(mockAuthenticationService.verifyUser(token)).thenReturn(dummyEmailConfirmResponse)
 
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v1/auth/verify")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("token", token)
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
+    }
+
+    @Test
+    fun `when user sign in triggered then expect success response`() {
+        val request = AuthenticationRequest(username = "mahmoud-darwoush", password = "123456")
+        val response = AuthenticationResponse(accessToken = "acess123", refreshToken = "refresh123")
+        `when`(mockAuthenticationService.signIn(request)).thenReturn(response)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/sign-in")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.accessToken").value(response.accessToken))
+    }
+
+    @Test
+    fun `when user password reset triggered then expect success response`() {
+        val email = "mahmoud@aol.com"
+        `when`(mockAuthenticationService.requestPasswordReset(email)).thenReturn(dummyEmailConfirmResponse)
+
+        val resultActions: ResultActions = mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v1/auth/password-reset")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("email", email)
+        )
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk)
+        resultActions.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.message").value(dummyEmailConfirmResponse.message))
+    }
 }
