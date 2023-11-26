c883c53e823468b0580698613a462e95469bb911
Fahri Can <fahri-can@gmx.at>
2023-11-26 15:22:13 +0100

    chore: Restructure method to get rif of static call in order to write better Unit Test.

--- src/main/kotlin/com/onecosys/getthingsdone/task/util/AuthenticatedUserProvider.kt
+++ src/main/kotlin/com/onecosys/getthingsdone/task/util/AuthenticatedUserProvider.kt
@@ -1,14 +1,29 @@
 package com.onecosys.getthingsdone.task.util
 
 import com.onecosys.getthingsdone.user.model.entity.User
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 
+
+interface SecurityContextProvider {
+
+    fun getCurrentUserAuthentication(): Authentication?
+}
+
 @Component
-class AuthenticatedUserProvider {
+class SpringSecurityContextProvider : SecurityContextProvider {
+
+    override fun getCurrentUserAuthentication(): Authentication? {
+        return SecurityContextHolder.getContext().authentication
+    }
+}
+
+@Component
+class AuthenticatedUserProvider(private val securityContextProvider: SecurityContextProvider) {
 
     fun getUser(): User {
-        val authentication = SecurityContextHolder.getContext().authentication
-        return authentication.principal as User
+        val authentication = securityContextProvider.getCurrentUserAuthentication()
+        return authentication?.principal as User
     }
 }
