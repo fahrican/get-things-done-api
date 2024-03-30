1b02e0343fec84232ec07c4cdcafa45c95e3e62d
Fahri Can <fahri-can@gmx.at>
2024-03-30 21:57:52 +0100

    chore: Making test file smaller by getting rid of the annotations.

--- src/test/kotlin/com/onecosys/getthingsdone/authentication/service/EmailServiceImplTest.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/authentication/service/EmailServiceImplTest.kt
@@ -2,31 +2,24 @@ package com.onecosys.getthingsdone.authentication.service
 
 import com.onecosys.getthingsdone.error.SignUpException
 import com.onecosys.getthingsdone.user.entity.User
-import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import io.mockk.verify
 import jakarta.mail.MessagingException
 import jakarta.mail.internet.MimeMessage
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.mail.javamail.JavaMailSender
 
 
-@ExtendWith(MockKExtension::class)
 internal class EmailServiceImplTest {
 
-    @RelaxedMockK
-    private lateinit var mockMailSender: JavaMailSender
+    private val mockMailSender = mockk<JavaMailSender>(relaxed = true)
 
-    @RelaxedMockK
-    private lateinit var mockMimeMessage: MimeMessage
+    private val mockMimeMessage = mockk<MimeMessage>(relaxed = true)
 
-    private lateinit var objectUnderTest: EmailService
+    private val objectUnderTest = EmailServiceImpl(mockMailSender)
 
     private val dummyToken = "a12b34c56"
 
@@ -39,15 +32,10 @@ internal class EmailServiceImplTest {
     )
 
 
-    @BeforeEach
-    fun setUp() {
-        MockKAnnotations.init(this)
-        objectUnderTest = EmailServiceImpl(mockMailSender)
-    }
-
     @Test
     fun `when send email verification is triggered then expect email successfully send`() {
         every { mockMailSender.createMimeMessage() } returns mockMimeMessage
+        every { mockMailSender.send(any<MimeMessage>()) } returns Unit
 
         objectUnderTest.sendVerificationEmail(user, dummyToken)
 
@@ -57,13 +45,13 @@ internal class EmailServiceImplTest {
 
     @Test
     fun `when send email verification is triggered then expect messaging exception`() {
-        every { mockMailSender.send(any<MimeMessage>()) } throws MessagingException("Simulated error")
+        every { mockMailSender.createMimeMessage() } throws SignUpException("failed to send email")
 
         val actualException = assertThrows<SignUpException> { objectUnderTest.sendVerificationEmail(user, dummyToken) }
 
         assertEquals("failed to send email", actualException.message)
         verify(exactly = 1) { mockMailSender.createMimeMessage() }
-        verify(exactly = 1) { mockMailSender.send(any<MimeMessage>()) }
+        verify(exactly = 0) { mockMailSender.send(any<MimeMessage>()) }
     }
 
     @Test
