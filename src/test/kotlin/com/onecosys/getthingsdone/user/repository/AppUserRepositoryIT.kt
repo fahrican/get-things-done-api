a421070905d58c93d4b9c02552ef73ca6a770bf5
Fahri Can <fahri-can@gmx.at>
2024-05-01 20:56:31 +0200

    chore: Extract logic to global level.

--- src/test/kotlin/com/onecosys/getthingsdone/user/repository/AppUserRepositoryIT.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/user/repository/AppUserRepositoryIT.kt
@@ -1,9 +1,11 @@
 package com.onecosys.getthingsdone.user.repository
 
 import com.onecosys.getthingsdone.user.entity.AppUser
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -15,47 +17,49 @@ class AppUserRepositoryIT @Autowired constructor(
     val appUserRepository: AppUserRepository
 ) {
 
+    val userEmail = "faris.diab@example.com"
+    val username = "faris-diab"
+    val user = AppUser(
+        firstName = "Faris",
+        lastName = "Diab",
+        email = userEmail,
+        appUsername = username,
+        appPassword = "SecurePassword123",
+        isVerified = true
+    )
+
+    @BeforeEach
+    fun setup() {
+        entityManager.persist(user)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        entityManager.clear()
+    }
+
+
     @Test
     fun `when new user gets persisted then check if email can be found`() {
-        val userEmail = "faris.diab@example.com"
-        val user = AppUser(
-            firstName = "Faris",
-            lastName = "Diab",
-            email = userEmail,
-            appUsername = "faris-diab",
-            appPassword = "SecurePassword123",
-            isVerified = true
-        )
-        entityManager.persist(user)
         entityManager.flush()
 
-        val foundUser = appUserRepository.findByEmail(userEmail)
+        val actualUser: AppUser? = appUserRepository.findByEmail(userEmail)
 
-        assertNotNull(foundUser)
-        assertEquals(userEmail, foundUser?.email)
+        assertNotNull(actualUser)
+        assertEquals(userEmail, actualUser?.email)
     }
 
     @Test
     fun `when user email does not exist then check if user is null`() {
         val email = "none-existent@example.com"
 
-        val foundUser = appUserRepository.findByEmail(email)
+        val actualUser: AppUser? = appUserRepository.findByEmail(email)
 
-        assertNull(foundUser)
+        assertNull(actualUser)
     }
 
     @Test
     fun `when new user gets persisted then check if username can be found`() {
-        val username = "nancy-ajram"
-        val user = AppUser(
-            firstName = "Nancy",
-            lastName = "Ajram",
-            email = "nancy.ajram@example.com",
-            appUsername = username,
-            appPassword = "SecurePassword456",
-            isVerified = true
-        )
-        entityManager.persist(user)
         entityManager.flush()
 
         val foundUser = appUserRepository.findByAppUsername(username)
