31409d021939d6546865981438ff448bf75a98dd
Fahri Can <fahri-can@gmx.at>
2024-05-01 20:24:24 +0200

    chore: Extract some logic to global.

--- src/test/kotlin/com/onecosys/getthingsdone/task/repository/TaskRepositoryIT.kt
+++ src/test/kotlin/com/onecosys/getthingsdone/task/repository/TaskRepositoryIT.kt
@@ -2,9 +2,11 @@ package com.onecosys.getthingsdone.task.repository
 
 import com.onecosys.getthingsdone.task.entity.Task
 import com.onecosys.getthingsdone.user.entity.AppUser
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
@@ -23,20 +25,23 @@ class TaskRepositoryIT @Autowired constructor(
         appUsername = "johndoe",
         appPassword = "securepassword"
     )
+    val expectedDescription = "Test Task"
+
+    val task = Task().apply { description = expectedDescription; appUser = user }
+
+    @BeforeEach
+    fun setup() {
+        entityManager.persist(user)
+        entityManager.persist(task)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        entityManager.clear()
+    }
 
     @Test
     fun `when find task by id and user is queried then expect pre-defined description`() {
-        val user = AppUser(
-            firstName = "John",
-            lastName = "Doe",
-            email = "john.doe@example.com",
-            appUsername = "johndoe",
-            appPassword = "securepassword"
-        )
-        entityManager.persist(user)
-        val expectedDescription = "Test Task"
-        val task = Task().apply { description = expectedDescription; appUser = user }
-        entityManager.persist(task)
         entityManager.flush()
 
         val actualTask = taskRepository.findTaskByIdAndUser(task.id, user)
@@ -54,11 +59,7 @@ class TaskRepositoryIT @Autowired constructor(
             appUsername = "janedoe",
             appPassword = "securepassword"
         )
-        entityManager.persist(user)
         entityManager.persist(anotherUser)
-        val expectedDescription = "Test Task"
-        val task = Task().apply { description = expectedDescription; appUser = user }
-        entityManager.persist(task)
         entityManager.flush()
 
         val actualTask = taskRepository.findTaskByIdAndUser(task.id, anotherUser)
@@ -67,7 +68,7 @@ class TaskRepositoryIT @Autowired constructor(
     }
 
     @Test
-    fun `when does description exists is queried then expect true`() {
+    fun `when does description exist is queried then expect true`() {
         val expectedDescription = "Unique Description"
         val task = Task().apply { description = expectedDescription }
         entityManager.persist(task)
@@ -79,7 +80,7 @@ class TaskRepositoryIT @Autowired constructor(
     }
 
     @Test
-    fun `when does description exists is queried then expect false`() {
+    fun `when does description exist is queried then expect false`() {
         val expectedDescription = "Unique Description"
 
         val actualResult = taskRepository.doesDescriptionExist(expectedDescription)
@@ -89,14 +90,6 @@ class TaskRepositoryIT @Autowired constructor(
 
     @Test
     fun `test findAllByUserAndIsTaskOpenOrderByIdAsc`() {
-        // Arrange
-        val user = AppUser(
-            firstName = "Alice",
-            lastName = "Smith",
-            email = "alice.smith@example.com",
-            appUsername = "alicesmith",
-            appPassword = "securepassword"
-        )
         entityManager.persist(user)
         val task1 = Task().apply { description = "Task 1"; isTaskOpen = true; appUser = user }
         val task2 = Task().apply { description = "Task 2"; isTaskOpen = false; appUser = user }
@@ -104,10 +97,8 @@ class TaskRepositoryIT @Autowired constructor(
         entityManager.persist(task2)
         entityManager.flush()
 
-        // Act
         val tasks = taskRepository.findAllByUserAndIsTaskOpenOrderByIdAsc(user, true)
 
-        // Assert
         assert(tasks.size == 1)
         assert(tasks.first().description == "Task 1")
     }
